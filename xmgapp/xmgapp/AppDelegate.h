07702d431cbaa3a64274797d7dff7bf4352a6ebf
xmg <xmg@163.com>
2016-04-15 10:58:29 +0800

    初始化项目

---
+++ xmgapp/xmgapp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xmgapp
+//
+//  Created by 贾明超 on 16/4/15.
+//  Copyright © 2016年 jmc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xmgapp/xmgapp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  xmgapp
+//
+//  Created by 贾明超 on 16/4/15.
+//  Copyright © 2016年 jmc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
